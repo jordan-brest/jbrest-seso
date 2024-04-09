60aa50385d07c1a153ea0b938617933a2d23fb1f
Jordan Brest <jordanbrest@MacBook-Pro-2.local>
2024-04-08 20:07:41 -0700

    Refactor to store promise in heap

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -12,29 +12,32 @@ module.exports = (logSources, printer) => {
         const node = {
           logEntry,
           logSource,
+          promise: logSource.popAsync(),
           weight: logEntry.date
         };
         queue.add(node);
       });
       return prom;
     });
+
+    //Wait for all log sources to return before draining the heap
     Promise.all(promises).then(() => {
       //Print function
       function print() {
         printer.done();
         resolve(console.log("Async sort complete."));
       }
-
       //Function to async drain heap until empty
       function asyncPopQueue() {
         if (queue.size() > 0) {
           const node = queue.remove();
           if (node.logEntry) printer.print(node.logEntry);
-          return node.logSource.popAsync().then((logEntry) => {
+          return node.promise.then((logEntry) => {
             if (logEntry) { 
               const item = {
                 logEntry,
                 logSource: node.logSource,
+                promise: node.logSource.popAsync(),
                 weight: logEntry.date
               };
               queue.add(item);
